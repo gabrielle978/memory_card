da7436982ffda83f9ae3939c8d31a52a2333f783
gabrielle978 <gbassiquete@outlook.com>
2024-10-23 19:34:22 -0300

    Upload Memory-Card Game

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -24,7 +24,7 @@ for(let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
-    box.onClick = handleClick;
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
 
@@ -40,5 +40,17 @@ function handleClick(){
 }
 
 function checkMatch(){
-
-}
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+      } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+      }
+    
+      openCards = [];
+    
+      if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu !");
+      }
+    }
